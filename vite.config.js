e4ef2ed815f2d86a092d7950ccdf498a7a5a8d6f
kundu <kundu.sourodip@outlook.com>
2024-06-15 22:05:08 +0530

    added url

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ||
+  'https://react-jobs-backend-v9m2.onrender.com/api';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://react-jobs-backend-v9m2.onrender.com/api/',
+        target: BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
